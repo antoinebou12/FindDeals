a044c268e4183d5363841cd98c60647b5bf0c93d
Antoine Boucher <antoine.bou13@gmail.com>
2023-02-15 03:51:27 +0000

    better filtering

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import email
 from html import escape
+import requests
+from bs4 import BeautifulSoup
 
 
 def eml_to_html(eml_file):
@@ -15,16 +17,26 @@ def eml_to_html(eml_file):
         if part.get_content_type() == "text/html":
             html = part.get_payload(decode=True).decode("utf-8")
 
-    # Escape HTML special characters
-    escaped_html = escape(html)
-
-    return escaped_html
+    return html
 
 
 def html_extract_table(html):
-    pass
-     
+    soup = BeautifulSoup(html, 'html.parser')
+
+    soup.prettify()
+
+    # td class outlook-fallback-font-family-receipt ereceipt-font-size
+    table = soup.find('td', attrs={'class': 'outlook-fallback-font-family-receipt ereceipt-font-size'})
+    print(table)
+
+    for row in table.find_all('tr'):
+        for cell in row.find_all('td'):
+            print(cell.text)
+
+            
+
 
 
 if __name__ == "__main__":
     html = eml_to_html("./eml/2023-01-30|19:32.eml")
+    html_extract_table(html)
