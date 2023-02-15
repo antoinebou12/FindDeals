68d6d22c337e7be45b47e227f1602ec8f49bda93
Antoine Boucher <antoine.bou13@gmail.com>
2023-02-15 04:07:32 +0000

    add templates

--- main.py
+++ main.py
@@ -1,8 +1,55 @@
+import os
 import email
 from html import escape
 import requests
 from bs4 import BeautifulSoup
 
+import imaplib
+import email
+
+from dotenv import load_dotenv
+
+
+# Load the environment variables from .env
+load_dotenv()
+
+
+# This modified function will download all emails from the specified sender, 
+# extract the HTML content from each one, find the table in each HTML content, 
+# and then do whatever processing you want with the table (e.g., write it to a file).
+
+def download_eml():
+    # Login credentials
+    # Get the values for the OUTLOOK_USERNAME and OUTLOOK_PASSWORD variables
+    username = os.getenv('OUTLOOK_USERNAME')
+    password = os.getenv('OUTLOOK_PASSWORD')
+
+    # Connect to the IMAP server
+    imap_server = "imap-mail.outlook.com"
+    imap_port = 993
+    mail = imaplib.IMAP4_SSL(imap_server, imap_port)
+
+    # Login to the email account
+    mail.login(username, password)
+
+    # Select the inbox folder
+    mail.select("inbox")
+
+    # Search for all emails from the specified sender
+    sender = "transaction@transaction-metro.ca"
+    result, data = mail.search(None, f'(FROM "{sender}")')
+
+    # Loop through the list of email ids and download each one as an eml file
+    for email_id in data[0].split():
+        result, data = mail.fetch(email_id, "(RFC822)")
+        raw_email = data[0][1]
+        email_message = email.message_from_bytes(raw_email)
+        file_name = f"{email_message['Subject']}.eml"
+        with open(file_name, "wb") as f:
+            f.write(raw_email)
+
+    # Logout from the email account
+    mail.logout()
 
 def eml_to_html(eml_file):
     # Read the EML file
@@ -33,10 +80,11 @@ def html_extract_table(html):
         for cell in row.find_all('td'):
             print(cell.text)
 
-            
+
 
 
 
 if __name__ == "__main__":
-    html = eml_to_html("./eml/2023-01-30|19:32.eml")
+    download_eml()
+    html = eml_to_html('Metro - Your receipt.eml')
     html_extract_table(html)
