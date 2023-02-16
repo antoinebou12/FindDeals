6004cd328690646ffc2ade0822b4e34474da9419
Antoine Boucher <antoine.bou13@gmail.com>
2023-02-16 02:17:47 +0000

    add data

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ from bs4 import BeautifulSoup
 import imaplib
 import email
 
+import pandas as pd
+import csv
+
+
 from dotenv import load_dotenv
 
 
@@ -14,15 +18,16 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-# This modified function will download all emails from the specified sender, 
-# extract the HTML content from each one, find the table in each HTML content, 
+# This modified function will download all emails from the specified sender,
+# extract the HTML content from each one, find the table in each HTML content,
 # and then do whatever processing you want with the table (e.g., write it to a file).
+
 
 def download_eml():
     # Login credentials
     # Get the values for the OUTLOOK_USERNAME and OUTLOOK_PASSWORD variables
-    username = os.getenv('OUTLOOK_USERNAME')
-    password = os.getenv('OUTLOOK_PASSWORD')
+    username = os.getenv("OUTLOOK_USERNAME")
+    password = os.getenv("OUTLOOK_PASSWORD")
 
     # Connect to the IMAP server
     imap_server = "outlook.office365.com"
@@ -51,6 +56,7 @@ def download_eml():
     # Logout from the email account
     mail.logout()
 
+
 def eml_to_html(eml_file):
     # Read the EML file
     with open(eml_file, "r") as f:
@@ -68,23 +74,36 @@ def eml_to_html(eml_file):
 
 
 def html_extract_table(html):
-    soup = BeautifulSoup(html, 'html.parser')
+    soup = BeautifulSoup(html, "html.parser")
 
     soup.prettify()
 
     # td class outlook-fallback-font-family-receipt ereceipt-font-size
-    table = soup.find('td', attrs={'class': 'outlook-fallback-font-family-receipt ereceipt-font-size'})
+    table = soup.find(
+        "td", attrs={"class": "outlook-fallback-font-family-receipt ereceipt-font-size"}
+    )
     print(table)
 
-    for row in table.find_all('tr'):
-        for cell in row.find_all('td'):
+    for row in table.find_all("tr"):
+        for cell in row.find_all("td"):
             print(cell.text)
 
 
+def parse_transaction_data():
 
+    categories = {
+        "EPICERIE": "Groceries",
+        "VIANDE": "Meat",
+        "CHARCUTERIE": "Charcuterie",
+        "MICHE": "Bakery",
+        "FROMAGE": "Cheese",
+        
+    }
 
 
 if __name__ == "__main__":
-    download_eml()
-    html = eml_to_html('Metro - Your receipt.eml')
-    html_extract_table(html)
+    html = eml_to_html("eml/Antoine, Your digital receipt has arrived.eml")
+    table = html_extract_table(html)
+    print(table)
+
+    parse_transaction_data()
